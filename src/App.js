4726ffcf531716d18e8b73c18f9d106ca665fd72
Aleksandra Baibik <abaibik@gmail.com>
2023-07-05 10:33:24 +0200

    fonts and settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="container-styles">
+        <nav></nav>
+        <header>
+          <h1>Coctailer</h1>
+          <p>Welcome to the site with cocktail recipes.</p>
+          <p>
+            Hier you can find some tasty recipe and improve your bartender
+            skills
+          </p>
+        </header>
+
+        <main></main>
+
+        <footer></footer>
+      </Container>
     </div>
   );
 }
