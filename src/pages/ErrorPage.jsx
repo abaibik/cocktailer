a8d74aeaca06231290615548f32e2631e7525cbb
Aleksandra Baibik <abaibik@gmail.com>
2023-07-06 23:30:11 +0200

    Add animation to ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import { useRouteError } from "react-router-dom";
 import "../index.css";
-import lottiePlayer from "@lottiefiles/lottie-player";
+import { Player } from "@lottiefiles/react-lottie-player";
 import { Container } from "semantic-ui-react";
 
 export default function ErrorPage() {
@@ -9,20 +9,29 @@ export default function ErrorPage() {
 
   return (
     <Container>
-      <h1>Oops!</h1>
-      <lottiePlayer
-        src="https://assets1.lottiefiles.com/packages/lf20_6s5vkp2g.json"
-        background="transparent"
-        speed="1"
-        style={{ width: "300px", height: "300px" }}
-        loop
-        controls
-        autoplay
-      ></lottiePlayer>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <div className="ui grid">
+        <div className="two column row">
+          <div className="column">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+              <i>{error.statusText || error.message}</i>
+            </p>
+          </div>
+
+          <div className="column">
+            <Player
+              src="https://assets1.lottiefiles.com/packages/lf20_6s5vkp2g.json"
+              background="transparent"
+              speed="1"
+              style={{ width: "30rem", height: "30rem", margin: "0" }}
+              loop
+              controls
+              autoplay
+            ></Player>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
