b38b6d3bbe1a67f2c4debc9e0125bec45d7ab4dc
Aleksandra <abaibik@gmail.com>
2024-03-17 12:03:17 +0100

    fix navbar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,8 @@
 import { Container } from "semantic-ui-react";
 import "../index.css";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Root() {
-  const { pathname } = useLocation();
   const homePath = "/";
   const searchCocktailPath = "/cocktails";
   const addGlassPath = "/addGlass";
@@ -13,25 +12,15 @@ export default function Root() {
       <Container>
         <div className="container-flex">
           <nav className="sidebar">
-            <Link
-              className="active item nav-item"
-              active={pathname === homePath}
-              to={homePath}
-            >
+            <Link className="nav-item" to={homePath}>
               Home
             </Link>
-            <Link
-              className="active item nav-item"
-              active={pathname === searchCocktailPath}
-              to={searchCocktailPath}
-            >
+
+            <Link className="nav-item" to={searchCocktailPath}>
               Search cocktail
             </Link>
-            <Link
-              className="active item nav-item"
-              active={pathname === addGlassPath}
-              to={addGlassPath}
-            >
+
+            <Link className="nav-item" to={addGlassPath}>
               Add glass
             </Link>
           </nav>
